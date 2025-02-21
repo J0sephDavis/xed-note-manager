bb9708d163bf260856c6d1df0b8387f5cb2de8b9
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-02-21 13:51:05 -0500

    load config file on init and save on demand.

--- JD__main_config.py
+++ JD__main_config.py
@@ -8,23 +8,31 @@ from gi.repository import GLib
 class JDPluginConfig():
 	def __init__(self, config_file_path:str):
 		self.user_home_dir = os.getenv('HOME')
-		self.path = config_file_path
+		self.path = config_file_path + 'xed_JDplugin.conf'
+		self.yaml = None
 		self._loadConfig()
 
 	def _loadConfig(self):
-		return
-		self.yaml_str = readYAML(self.path)
-		JD_EntLibrary.regex_filter = re.compile('*')
-		print(f'{DEBUG_PREFIX} config yaml_str: {self.yaml_str}')
-		# ...set class properties...
-		pass
+		print(f'{DEBUG_PREFIX} Loading configuration file ({self.path})')
+		file:Gio.File = getFileFromPath(self.path)
+		if (file.query_exists()): self.yaml = readYAML(self.path)
+		if self.yaml is not None:
+			print(f'{DEBUG_PREFIX} config yaml found: {self.yaml}')
+			return
+		print(f'{DEBUG_PREFIX} config does not exist, yet.')
+		self.yaml = {
+			"notes_directory" : f'{self.user_home_dir}/Documents/Notes',
+		}
 
-	def saveConfig(self, config_file_path):
-		writeYAML(self.path, self.yaml_str)
-		pass
-
-	def GetLibraryPath(self): # TODO support multiple paths
-		return f'{self.user_home_dir}/Documents/Notes'
+	def saveConfig(self):
+		print(f'{DEBUG_PREFIX} saveConfig:\n{self.yaml}\n{self.yaml.__str__()}')
+		file:Gio.File = getFileFromPath(self.path)
+		if (file.query_exists()):
+			file.delete()
+		outputStream:Gio.FileOutputStream = file.create(Gio.FileCreateFlags.REPLACE_DESTINATION, None)
+		yaml_bytes = bytearray(yaml.dump(self.yaml, explicit_start=True,explicit_end=False) + '---', encoding='utf-8')
+		outputStream.write_all(yaml_bytes)
+		outputStream.close()
 
 class JD_EntBase():
 	def __init__(self, fileInfo:Gio.FileInfo):
