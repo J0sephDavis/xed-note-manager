76bee53d755aeebdbd2ea037352b16410735880e
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-02-23 03:00:48 -0500

    Config can hold multiple libraries

--- JD__main_config.py
+++ JD__main_config.py
@@ -3,47 +3,71 @@ from gi.repository import Gtk
 import os
 
 class JDPluginConfig():
+	user_home_dir = os.getenv('HOME')
+
 	def __init__(self, config_file_path:str):
-		self.user_home_dir = os.getenv('HOME')
+		print(f'{DEBUG_PREFIX} JDPluginConfig.__init__')
 		self.path = config_file_path + 'xed_JDplugin.conf'
+		self.widget = None
 		self.yaml = None
+		self.libraries = []
 		self._loadConfig()
 
-	def GetLibraryPath(self): # TODO support multiple paths
-		return self.yaml['notes_directory']
+	def GetLibraries(self) -> List[str]: # TODO support multiple paths
+		return self.yaml['notes_directories']
 
 	def _loadConfig(self):
+		self.yaml = None
 		print(f'{DEBUG_PREFIX} Loading configuration file ({self.path})')
 		file:Gio.File = getFileFromPath(self.path)
 		if (file.query_exists()): self.yaml = readYAML(self.path)
-		if self.yaml is not None:
-			print(f'{DEBUG_PREFIX} config yaml found: {self.yaml}')
+		if self.yaml is None:
+			print(f'{DEBUG_PREFIX} config does not exist. Creating')
+			self.yaml = {
+			"notes_directories" : [f'{self.user_home_dir}/Documents/Notes'],
+			}
 			return
-		print(f'{DEBUG_PREFIX} config does not exist, yet.')
-		self.yaml = {
-			"notes_directory" : f'{self.user_home_dir}/Documents/Notes',
-		}
+		# ---
+		print(f'{DEBUG_PREFIX} config:  {type(self.yaml)}\n{self.yaml}')
+		# ---
+		# self.libraries = []
+		for dir in self.yaml["notes_directories"]:
+			print(f'{DEBUG_PREFIX} loadConfig, append dir: {dir}')
+			self.libraries.append(dir)
 
 	def saveConfig(self,action=None):
-		print(f'{DEBUG_PREFIX} saveConfig:\n{self.yaml}\n{self.yaml.__str__()}')
+		print(f'{DEBUG_PREFIX} saveConfig:\n{self.yaml}')
 		file:Gio.File = getFileFromPath(self.path)
 		if (file.query_exists()):
 			file.delete()
+		buffer = self.libraryPaths_GtkTextView.get_buffer()
+		buff_text:List[str] = buffer.get_text(buffer.get_start_iter(),buffer.get_end_iter(),False).splitlines()
+		self.libraries = []
+		for line in buff_text:
+			print(f'{DEBUG_PREFIX} saveConfig, append dir {line}')
+			self.libraries.append(line)
+		self.yaml['notes_directories'] = self.libraries
 		outputStream:Gio.FileOutputStream = file.create(Gio.FileCreateFlags.REPLACE_DESTINATION, None)
 		yaml_bytes = bytearray(yaml.dump(self.yaml, explicit_start=True,explicit_end=False) + '---', encoding='utf-8')
 		outputStream.write_all(yaml_bytes)
 		outputStream.close()
 
-	def createConfigureWidget(self):
-		widget_vbox = Gtk.Box(spacing=6, orientation=Gtk.Orientation.VERTICAL)
+	def _createWidget(self):
+		self.widget = Gtk.Box(spacing=6, orientation=Gtk.Orientation.VERTICAL)
 		# --------------
-		row_notes_dir = Gtk.Box(spacing=6,orientation=Gtk.Orientation.HORIZONTAL)
-		notes_dir_path_label = Gtk.Label(label=self.GetLibraryPath())
-		notes_dir_file_browser = Gtk.Label(label="TODO") # spawn a text entry with a button for FileChooserDialog? 
+		# row_notes_dir = Gtk.Box(spacing=6,orientation=Gtk.Orientation.HORIZONTAL)
+		# notes_dir_path_label = Gtk.Label(label=self.GetLibraryPath())
+		self.libraryPaths_GtkTextView = Gtk.TextView()
+		text = self.yaml['notes_directories']
+		if text is None:
+			text = ''
+		else:
+			text = '\n'.join(text)
+		self.libraryPaths_GtkTextView.get_buffer().set_text(text)
 		# ------
-		row_notes_dir.pack_start(Gtk.Label(label="notes_dir"),True,True,0)
-		row_notes_dir.pack_start(notes_dir_path_label,True,True,0)
-		row_notes_dir.pack_start(notes_dir_file_browser,True,True,0)
+		self.widget.pack_start(Gtk.Label(label="notes_dir"),True,True,0)
+		# row_notes_dir.pack_start(notes_dir_path_label,True,True,0)
+		self.widget.pack_start(self.libraryPaths_GtkTextView,True,True,0) # TOD request more size!
 		# --------------
 		row_file_regex = Gtk.Box(spacing=6,orientation=Gtk.Orientation.HORIZONTAL)
 		file_regex_entry = Gtk.Entry()
@@ -55,9 +79,12 @@ class JDPluginConfig():
 		save_button = Gtk.Button.new_with_label("Save")
 		save_button.connect("clicked",self.saveConfig)
 		# --------------
-		widget_vbox.pack_start(row_notes_dir,True,True,0)
-		widget_vbox.pack_start(row_file_regex,True,True,0)
-		widget_vbox.pack_start(save_button,True,True,0)
+		# widget_vbox.pack_start(row_notes_dir,True,True,0)
+		self.widget.pack_start(row_file_regex,True,True,0)
+		self.widget.pack_start(save_button,True,True,0)
 		# --------------
 		# TODO button with callback connected to saveConfig
-		return widget_vbox
+
+	def createConfigureWidget(self):
+		if self.widget is None: self._createWidget()
+		return self.widget
