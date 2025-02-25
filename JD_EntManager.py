65b2c0de34b3b50b6848b15bf1c9011e7b4dcce7
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-02-24 22:52:06 -0500

    create Entity Manager

---
+++ JD_EntManager.py
@@ -0,0 +1,59 @@
+from JD__entities import *
+import sys
+import weakref
+class JD_EntTracker(): # TODO better name
+	libraries:List[JD_EntLibrary] = []
+	notes_weak:List[JD_EntNote] = []
+	discard_pile:List[JD_EntLibrary] = [] # weak refs to every library we have removed. for debugging
+
+	subscribers_library_removed = [] # try weak refs here. no sense in keeping an object in existence because it has a callback attached.
+	subscribers_library_added = []
+
+	def __init__(self):
+		pass
+	
+	def deactivate(self):
+		self.subscribers_library_removed.clear()
+		self.subscribers_library_added.clear()
+		self.libraries.clear()
+		self.discard_pile.clear()
+		self.notes_weak.clear()
+# ------------------------------ properties -------------------------------------
+	def GetLibraries(self): return self.libraries
+	def GetNotes(self): return self.notes_weak
+# ------------------------------ callbacks -------------------------------------
+	def libraryAddedCallback(self, library_path:str):
+		print(f'{DEBUG_PREFIX} libraryAddedCallback: {library_path}')
+		library = JD_EntLibrary(library_path)
+		self.libraries.append(library)
+		self.announceLibraryAdded(library)
+
+	def libraryRemovedCallback(self, library_path:str):
+		print(f'{DEBUG_PREFIX} libraryRemovedCallback: {library_path}')
+		removal:List[JD_EntLibrary] = self.libraries.filter(lambda library: library.path == library_path)
+		print(f'{DEBUG_PREFIX} REMOVAL[]: {removal}')
+		for library in removal:
+			self.libraries.remove(library)
+			self.announceLibraryRemoved(library)
+			print(f'{DEBUG_PREFIX} refcount of library({library.path}): {sys.getrefcount(library)}')
+			self.discard_pile.append(weakref.ref(library))
+# --------------------------------- events ----------------------------------------
+	def announceLibraryRemoved(self, library:JD_EntLibrary):
+		for callback in self.subscribers_library_removed:
+			callback(library)
+	
+	def announceLibraryAdded(self,library:JD_EntLibrary):
+		for callback in self.subscribers_library_added:
+			callback(library)
+
+	def subscribeLibraryAdded(self, callback): 
+		if not callable(callback):
+			print(f'{DEBUG_PREFIX} subscribeLibraryAdded, callback is not callable.')
+			return
+		self.subscribers_library_added.append(weakref.ref(callback))
+
+	def subscribeLibraryRemoved(self, callback): 
+		if not callable(callback):
+			print(f'{DEBUG_PREFIX} subscribeLibraryRemoved, callback is not callable.')
+			return
+		self.subscribers_library_removed.append(weakref.ref(callback))
