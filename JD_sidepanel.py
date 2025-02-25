f8c4be8f53086f6c28ec1d8d5cd4e4bcac1304f3
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-02-24 22:55:05 -0500

    Create panel widget on initialization of class. treeStore is no longer a class variable, use self.TreeView.get_model()

--- JD_sidepanel.py
+++ JD_sidepanel.py
@@ -7,7 +7,7 @@ from gi.repository import GLib
 from JD__entities import *
 from JD__utils import DEBUG_PREFIX
 from typing import List
-
+from JD_EntManager import *
 # (later)
 # - right click menu to choose whether a file shoudl be opened in a new tab, deleted, moved, &c
 # - select multiple notes and open/delete/perform some other action on them
@@ -23,20 +23,32 @@ def treeStorePrintRow(store,tPath,tIter):
 	print('\t' * (tPath.get_depth()-1), store[tIter][:], sep="")
 
 class JDPanelTab(Gtk.TreeView):
-	def __init__(self, window:Xed.Window):
+	def __init__(self, internal_name:str, display_name:str, icon_name:str, window:Xed.Window):
 		super().__init__()
+		self.internal_name = internal_name
+		self.display_name = display_name
+		self.icon_name = icon_name
 		print(f'{DEBUG_PREFIX} PanelTab __init__')
-		# self.library_nodes:List[Tuple[JD_EntLibrary,Gtk.TreeIter]] = [] # str:library name
-		# COLUMNS: filename,
-		self.treeStore:Gtk.TreeStore = Gtk.TreeStore(str, GObject.TYPE_PYOBJECT)
-		self.treeView:Gtk.TreeView = Gtk.TreeView(model=self.treeStore)
+		self.widget_container = Gtk.Box(spacing=6, orientation=Gtk.Orientation.VERTICAL)
+
+		treeStore:Gtk.TreeStore = Gtk.TreeStore(str, GObject.TYPE_PYOBJECT)
+		self.treeView:Gtk.TreeView = Gtk.TreeView(model=treeStore)
 		
 		self.treeView.insert_column(
 			Gtk.TreeViewColumn(title='name', cell_renderer=Gtk.CellRendererText(),text=0),
 			position=-1
 		)
+		
 		# https://lazka.github.io/pgi-docs/Gtk-3.0/classes/TreeView.html#signals
 		self.treeView.connect("row-activated", self.handler_row_activated, window)
+
+		remove_item_button = Gtk.Button(label="Remove Selected")
+		remove_item_button.connect("clicked", self.handler_remove_selected)
+		self.widget_container.pack_start(remove_item_button,False,True,5)
+		self.widget_container.pack_start(self.treeView,True,True,0)
+		self.widget_container.show_all()
+
+	def GetWidget(self): return self.widget_container;
 
 	def handler_row_activated(self, treeview, path, col, window):
 		count_selection = treeview.get_selection().count_selected_rows()
@@ -63,7 +75,7 @@ class JDPanelTab(Gtk.TreeView):
 		print(f'{DEBUG_PREFIX} {type(entry)} SELECTED[0] {entry[0]} [1]: {entry[1]}')
 		if (type(entry[1]) is JD_EntLibrary):
 			print(f'{DEBUG_PREFIX} removing library {entry[0]}')
-			self.treeStore.remove(selected_iter)
+			self.treeView.get_model().remove(selected_iter)
 		else:
 			print(f'{DEBUG_PREFIX} selected entry is not a library {entry[0]}, {type(entry[1])}')
 			return
@@ -85,11 +97,9 @@ class JDPanelTab(Gtk.TreeView):
 
 	def addLibrary(self, library:JD_EntLibrary):
 		print(f'{DEBUG_PREFIX} PanelTab addLibrary')
-		node:Gtk.TreeIter = self.treeStore.append(None, [library.getFilename(), library])
-		# self.library_nodes.append((library,node))
-
+		node:Gtk.TreeIter = self.treeView.get_model().append(None, [library.getFilename(), library])
 		for note in library.notes:
-			self.treeStore.append(node, [note.getFilename(), note])
+			self.treeView.get_model().append(node, [note.getFilename(), note])
 	
 	def removeLibrary(self,library_path:str):
 		print(f'{DEBUG_PREFIX} PanelTab removeLibrary(library_path={library_path})')
