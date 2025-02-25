faa66c6420fe06f45d4e9e8dc3a9deba5132a3da
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-02-24 19:17:53 -0500

    Publish subscribe methods for JDPluginConfig to notify other classes about libraries being added or removed

--- JD__main_config.py
+++ JD__main_config.py
@@ -36,6 +36,20 @@ class JDPluginConfig():
 			print(f'{DEBUG_PREFIX} loadConfig, append dir: {dir}')
 			self.libraries.append(dir)
 
+	def SubscribeLibraryAdded(self, callback): 
+		self.library_added_callbacks.append(callback)
+
+	def SubscribeLibraryRemoved(self, callback): 
+		self.library_removed_callbacks.append(callback)
+
+	def EmitLibraryAdded(self, library_path:str):
+		for cb in self.library_added_callbacks:
+			cb(library_path)
+
+	def EmitLibraryRemoved(self, library_path:str):
+		for cb in self.library_removed_callbacks:
+			cb(library_path)
+
 	def saveConfig(self,action=None):
 		old_libraries = self.libraries
 		
