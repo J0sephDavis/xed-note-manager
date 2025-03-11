e827bea8e3c2fbb9d34276445edfcb163b3e0ea6
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-03-11 17:16:45 -0400

    avoid reopening notes that have already been opened in a window
    Sometimes the best solution is right infront of us

--- Entities/NLP_EntityNote.py
+++ Entities/NLP_EntityNote.py
@@ -1,28 +1,39 @@
 from NoteLibraryPlugin import DEBUG_PREFIX
 import gi
 gi.require_version('Xed', '1.0')
+gi.require_version('Gtk', '3.0')
 from gi.repository import Xed
+from gi.repository import Gtk
 from gi.repository import Gio
 from NLP_Utils import getFileFromPath, readYAML, OpenPathInFileExplorer
 import yaml
 from Entities.NLP_EntityBase import EBase
+from weakref import ref
+from typing import Dict,List,Tuple
+# BUG ---------------------------
+# open a note in a new tab
+# move the note to a new window
+# open the same note in a new tab (in the new window)
+# It will open a duplicate tab instead of referencing the current one
+# -------------------------------
 
 class ENote(EBase):
 	def __init__(self, file:Gio.File):
 		super().__init__(file=file)
+		self.file_read:bool = False # True ONLY if readYAML has been called already.
+		self.__yaml = None
 
 	@classmethod
 	def from_GFileInfo(cls, parent_dir:str, fileInfo:Gio.FileInfo):
 		path = f'{parent_dir}/{fileInfo.get_name()}'
 		return cls(getFileFromPath(path))
 
-	def open_in_new_tab(self, window:Xed.Window): # window is the main Xed window
-		# TODO, If window is already open, focus tab instead of opening a new one.
-		# Make configurable? Or accelerator defined, like ctrl+activate opens regardless.
-		window.create_tab_from_location(
-			self.file,
-			None,0,0,True
-		)
+	def open_in_new_tab(self, window:Xed.Window):
+		tab:Xed.Tab = window.get_tab_from_location(self.file)
+		if tab is None:
+			tab = window.create_tab_from_location(self.file,None,0,0,True)
+		window.set_active_tab(tab)
+		return tab
 	
 	def open_in_explorer(self):
 		OpenPathInFileExplorer(self.get_path().replace(self.get_filename(),''))
