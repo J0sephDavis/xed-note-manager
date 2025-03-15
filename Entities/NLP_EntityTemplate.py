dd121a95133c0d32b34082bacecfd32da502f464
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2025-03-14 20:22:54 -0400

    ETemplate partially implemented. generate_contents(mapping)

--- Entities/NLP_EntityTemplate.py
+++ Entities/NLP_EntityTemplate.py
@@ -3,45 +3,33 @@ from gi.repository import GObject
 from gi.repository import GLib
 from gi.repository import Gio
 from Entities.NLP_EntityBase import EBase
-from gi.repository import Gio
-from NLP_Utils import new_unique_file, GetFileContents
+from NLP_Utils import GetFileContents
 from typing import Callable, List
 import re
 # from Entities.NLP_EntityLibrary import ELibrary
 from NLP_Template import NLP_Template
-class ETemplate(EBase):
+class ETemplate(EBase): # TODO check if the file has been updated (maybe this is handled by ELibrary who monitors the dir)
 	def __init__(self, file:Gio.File):
 		super().__init__(file,None)
 		self.template:NLP_Template = None
-		self.data:bytes = None
 
 	def load_file(self):
 		if (self.template is None):
 			self.template = NLP_Template(GetFileContents(self.file))
 		return self.template
 	
-	# def ChooseFileName(self, library:ELibrary, make_unique:bool):
-	# 	pass
-	
-	def GetContents(self,value_map:dict[bytes,
-							# simple replacement
-							bytes |\
-							#def func(bytes)->bytes
-							Callable[[re.Match[bytes]],bytes]] | \
-							#def func(bytes,value_map:dict)->bytes
-							Callable[[re.Match,dict],bytes]
-						)->bytes:
-		raise NotImplementedError("GetContents not implemented")
-		if self.data is None: 
-			self.data = self.template.custom_safe_substitute(value_map)
-		return self.data
-	
-	# make_unique appends a number to the end. e.g, note, note 1, note 2, note 3...
-	def save_to_file(self,dir:Gio.File,make_unique:bool):
-		raise NotImplementedError("save_to_file not implemented")
-		if dir.exists():
-			if make_unique == False:
-				return False
-		# dir.
-		
+	def generate_filename(self):
 		pass
+	
+	def generate_contents(self,value_map:(dict[bytes,				# key value must be a byte string
+							# in addition to these, you could probably add
+							# ones that do not conform. So long as their
+							# KEY will never be matched. such as $sjdklf
+							# Then, that data can be accessed within the
+							# complex replacement
+							bytes | 								# simple replacement
+							Callable[[re.Match[bytes]],bytes]] |	# function replacement, func(bytes)->bytes
+							Callable[[re.Match,dict],bytes])		# complex replacement, func(bytes,mapping)->bytes
+						)->bytes:
+		if (self.template is None): self.load_file()
+		return self.template.custom_safe_substitute(value_map)
